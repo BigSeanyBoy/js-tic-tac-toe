d3fadd4a6ce590cb2010797534d816c52ea646e9
Sean Hammell <sean.m.hammell@gmail.com>
2022-03-29 19:50:21 -0500

    Add end game checks

--- script.js
+++ script.js
@@ -1,18 +1,60 @@
 const gameBoard = (() => {
-  const boardState = [1, 2, 'X', 'O', 5, 6, 7, 8, 9];
+  let boardState = Array(9).fill().map((_, i) => i + 1);
   let playerTurn = 1;
+
   const positionAvailable = (index) => {
     return typeof boardState[index] === 'number';
   }
+
   const updateBoardState = (index) => {
     if (positionAvailable(index)) {
       boardState[index] = playerTurn === 1 ? 'X' : 'O';
+      if (gameOver()) {
+        alert(`Player ${playerTurn} Wins!`);
+        boardState = Array(9).fill().map((_, i) => i + 1);
+        return;
+      }
       playerTurn = Math.abs(playerTurn - 2) + 1;
     }
     else {
       alert('That position is taken');
     }
   }
+
+  const gameOver = () => {
+    if (rowCheck() || colCheck() || diagCheck()) return true;
+    return false;
+  }
+
+  const rowCheck = () => {
+    for (let i = 0; i < 3; i++) {
+      const cell = i * 3;
+      if (boardState[cell] === boardState[cell + 1] 
+          && boardState[cell] === boardState[cell + 2]) {
+            return true;
+      }
+    }
+  }
+
+  const colCheck = () => {
+    for (let i = 0; i < 3; i++) {
+      const cell = i;
+      if (boardState[cell] === boardState[cell + 3] 
+          && boardState[cell] === boardState[cell + 6]) {
+            return true;
+      }
+    }
+  }
+
+  const diagCheck = () => {
+    if ((boardState[0] === boardState[4] 
+         && boardState[0] === boardState[8]) 
+        || (boardState[2] === boardState[4] 
+        && boardState[2] === boardState[6])) {
+          return true;
+    }
+  }
+
   const print = () => {
     const playerHeader = document.querySelector('.player-header');
     playerHeader.textContent = `Player ${playerTurn }'s Turn`;
@@ -39,12 +81,6 @@ const gameBoard = (() => {
 
   return {updateBoardState, print};
 })();
-
-const player = (gamePiece) => {
-  const getPiece = () => gamePiece;
-
-  return {getPiece};
-}
 
 const displayController = (() => {
   const run = (index) => {
